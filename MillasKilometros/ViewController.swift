78e4bf6a4952bcffa9f8e8e91a29c2ddfbd5ad63
AnderIpartek <anderipartek@urkos-mac.local>
2016-09-21 16:20:02 +0200

    Initial Commit

---
+++ MillasKilometros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MillasKilometros
+//
+//  Created by AnderIpartek on 21/9/16.
+//  Copyright © 2016 AnderIpartek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
